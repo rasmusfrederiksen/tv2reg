4de29eff192ad8eabf758ffa9d8dac43c926de78
Rasmus Frederiksen <rasmus@vector.dk>
2023-02-22 15:57:40 +0100

    Added location filtering

---
+++ src/components/LocationSelect.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <q-select
+      class="selectwide"
+      standout="bg-blue-grey-6 text-white"
+      v-model="location"
+      :options="locations"
+      label="Område"
+      dense="dense"
+      @update:model-value="value => $emit('update:modelValue', value)"
+    />
+  </div>
+</template>
+
+<style>
+  .selectwide {
+    min-width: 100px;
+  }
+</style>
+
+<script>
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'LocationSelect',
+    data () {
+      return {
+        locations: [
+          {
+            label: 'Alle',
+            value: ''
+          }
+        ],
+        location: {
+          label: 'Alle',
+          value: ''
+        }
+      }
+    },
+    created () {
+      fetch('https://public.lorry.bazo.dk/v1/taxonomies/locations')
+      .then((response) => response.json())
+      .then((data) => {
+        let p = data.data.map(e => ({ label: e.name, value: e.uuid }));
+        this.locations = this.locations.concat(p);
+      });
+    },
+    methods: {
+    },
+    emits: ['update:modelValue']
+  });
+</script>
--- src/pages/ArticleSearchPage.vue
+++ src/pages/ArticleSearchPage.vue
@@ -1,39 +1,60 @@
 <template>
   <q-page class="flex column flex-center">
     <div class="text-h3 q-pa-lg">Artikelsøgning</div>
-    <SearchField v-model="searchQuery" label="Søg efter artikler" class="search"/>
+    <div class="flex row hundred justify-center">
+      <SearchField v-model="searchQuery" label="Søg efter artikler" class="search q-pr-sm"/>
+      <LocationSelect v-model="location"/>
+    </div>
+    {{ location.value }}
     <ArticleList :url="searchUrl" />
   </q-page>
 </template>
 
 <style>
+  .hundred {
+    width: 100%;
+  }
   .search {
-    width: 80%;
+    width: calc(100% - 120px);
+    max-width: 400px;
   }
 </style>
 
 <script>
   import { defineComponent } from 'vue';
   import SearchField from 'components/SearchField.vue';
+  import LocationSelect from 'components/LocationSelect.vue';
   import ArticleList from 'components/ArticleList.vue';
 
   export default defineComponent({
     name: 'ArticleSearchPage',
     components: {
       SearchField,
+      LocationSelect,
       ArticleList
     },
     data() {
       return {
-        searchQuery: ''
+        searchQuery: '',
+        location: {}
       }
     },
     computed: {
+      locationFilterString () {
+        let uuid = (this.location?.value || '');
+        return `${uuid.length > 0 ? 'filter[location_uuid]=' : ''}${uuid}`;
+      },
+      searchFilterString () {
+        return `${this.searchQuery.length > 0 ? 'filter[title]=' : ''}${this.searchQuery}`;
+      },
+      filters () {
+        return `?${this.locationFilterString}&${this.searchFilterString}`
+      },
       searchUrl () {
         if (this.searchQuery.length === 0) {
           return '';
         } else {
-          return `https://public.lorry.bazo.dk/v1/articles?filter[title]=${this.searchQuery}`;
+          return `https://public.lorry.bazo.dk/v1/articles${this.filters}`;
         }
       }
     }
